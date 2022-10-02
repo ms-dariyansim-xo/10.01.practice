df4af5823b4511cd3039f7cd8e22518e3b429f4c
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-10-01 21:57:48 -0700

    Update practicejs.js

--- practicejs.js
+++ practicejs.js
@@ -8,3 +8,9 @@ numSongs = 5;
 
 //INITIALIZATION by declaring a variable and assigning a val
 let numAlbums = 20;
+
+//Variable can be assigned a value without declaration
+hitCount = 10;
+
+//Testing one of the variables (not part of the content)
+console.log('This is result of numAlbum' + numAlbums)
